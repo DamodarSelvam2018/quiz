b1a28bcc85624a240771f4d1e3cf182a76aafd92
Damodar Selvam <damodar.selvam@gmail.com>
2020-08-26 23:59:55 -0400

    first draft version

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'dart:io';
 //TODO: Step 2 - Import the rFlutter_Alert package here.
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
 import 'quiz_brain.dart';
+import 'themeColors.dart';
 
 QuizBrain quizBrain = QuizBrain();
+ThemeColors themeColors = ThemeColors();
 
 void main() => runApp(Quizzler());
 
@@ -18,6 +20,13 @@ class Quizzler extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
+        appBar: AppBar(
+          backgroundColor: Colors.grey.shade900,
+          title: Text(
+            'Manners matters',
+            style: TextStyle(color: themeColors.kombuGreen[900]),
+          ),
+        ),
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -41,57 +50,22 @@ class _QuizPageState extends State<QuizPage> {
 
   void checkAnswer(String userPickedAnswer) {
     //bool correctAnswer = quizBrain.getCorrectAnswer();
-
+    String choice = quizBrain.checkAnswer(userPickedAnswer);
     setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
-      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
-      if (quizBrain.isFinished() == true) {
-        //TODO Step 4 Part A - show an alert using rFlutter_alert,
-
-        //This is the code for the basic alert from the docs for rFlutter Alert:
-        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
-
-        //Modified for our purposes:
+      if (choice == "gold" && quizBrain.getTotalQuestionsAsked() > 1) {
         Alert(
             context: context,
-            title: 'Finished!',
-            desc: 'You\'ve reached the end of the quiz.',
+            title: 'Yay!',
+            desc: 'You\'ve Scored a gold',
+            content: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.star,
+                  color: Colors.yellowAccent,
+                )
+              ],
+            ),
             buttons: [
-              DialogButton(
-                child: Text("Home"),
-                onPressed: () {
-                  //quizBrain.nextQuestion();
-                  quizBrain.reset();
-
-                  Navigator.pop(context);
-                },
-              )
-            ]).show();
-
-        //TODO Step 4 Part C - reset the questionNumber,
-        quizBrain.reset();
-
-        //TODO Step 4 Part D - empty out the scoreKeeper.
-        // scoreKeeper = [];
-      }
-
-      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
-      else {
-        if (userPickedAnswer == quizBrain.getGoldAnswer()) {
-          myChoice = "gold";
-          Alert(
-              context: context,
-              title: 'Yay!',
-              desc: 'You\'ve Scored a gold',
-              content: Column(
-                children: <Widget>[
-                  Icon(
-                    Icons.star,
-                    color: Colors.yellowAccent,
-                  )
-                ],
-              ),
-              buttons: [
 //                DialogButton(
 //                  child: Text("Prev"),
 //                  onPressed: () {
@@ -99,232 +73,270 @@ class _QuizPageState extends State<QuizPage> {
 //                    Navigator.pop(context);
 //                  },
 //                ),
-                DialogButton(
-                  child: Text("Next"),
-                  onPressed: () {
-                    //quizBrain.nextQuestion();
-                    Navigator.pop(context);
-                  },
+              DialogButton(
+                child: Text("Next"),
+                onPressed: () {
+                  //quizBrain.nextQuestion();
+                  Navigator.pop(context);
+                },
+              )
+            ]).show();
+      } else if (choice == "silver" && quizBrain.getTotalQuestionsAsked() > 1) {
+        Alert(
+            context: context,
+            title: 'Wow!',
+            desc: 'You\'ve Scored a Silver',
+            content: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.star,
+                  color: Colors.grey,
                 )
-              ]).show();
-        } else if (userPickedAnswer == quizBrain.getSilverAnswer()) {
-          myChoice = "silver";
-          Alert(
-              context: context,
-              title: 'Wow!',
-              desc: 'You\'ve Scored a Silver',
-              content: Column(
-                children: <Widget>[
-                  Icon(
-                    Icons.star,
-                    color: Colors.grey,
-                  )
-                ],
-              ),
-              buttons: [
-                DialogButton(
-                  child: Text("Next"),
-                  onPressed: () {
-                    //quizBrain.nextQuestion();
-                    Navigator.pop(context);
-                  },
+              ],
+            ),
+            buttons: [
+              DialogButton(
+                child: Text("Next"),
+                onPressed: () {
+                  //quizBrain.nextQuestion();
+                  Navigator.pop(context);
+                },
+              )
+            ]).show();
+      } else if (choice == "bronze" && quizBrain.getTotalQuestionsAsked() > 1) {
+        Alert(
+            context: context,
+            title: 'Good!',
+            desc: 'You\'ve Scored a Bronze',
+            content: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.star,
+                  color: Colors.orange,
                 )
-              ]).show();
-        } else if (userPickedAnswer == quizBrain.getBronzeAnswer()) {
-          myChoice = "bronze";
-          Alert(
-              context: context,
-              title: 'Good!',
-              desc: 'You\'ve Scored a Bronze',
-              content: Column(
-                children: <Widget>[
-                  Icon(
-                    Icons.star,
-                    color: Colors.orange,
-                  )
-                ],
-              ),
-              buttons: [
-                DialogButton(
-                  child: Text("Next"),
-                  onPressed: () {
-                    //quizBrain.nextQuestion();
-                    Navigator.pop(context);
-                  },
-                )
-              ]).show();
-        }
-        quizBrain.nextQuestion();
+              ],
+            ),
+            buttons: [
+              DialogButton(
+                child: Text("Next"),
+                onPressed: () {
+                  //quizBrain.nextQuestion();
+                  Navigator.pop(context);
+                },
+              )
+            ]).show();
       }
+      //sleep(const Duration(seconds: 5));
+      if (quizBrain.getTotalQuestionsAsked() <=
+          quizBrain.questionBankLength()) {
+        quizBrain.nextQuestion();
+      } else {
+        Alert(
+            context: context,
+            title: 'Finished!',
+            desc: 'You\'ve reached the end of the quiz.',
+            buttons: [
+              DialogButton(
+                child: Text("Home"),
+                onPressed: () => Navigator.pop(context),
+              )
+            ]).show();
+        quizBrain.reset();
+      }
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
+      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
+//      if (quizBrain.isFinished() == true) {
+//        //TODO Step 4 Part A - show an alert using rFlutter_alert,
+//
+//        //This is the code for the basic alert from the docs for rFlutter Alert:
+//        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
+//
+//        //Modified for our purposes:
+//        Alert(
+//            context: context,
+//            title: 'Finished!',
+//            desc: 'You\'ve reached the end of the quiz.',
+//            buttons: [
+//              DialogButton(
+//                child: Text("Home"),
+//                onPressed: () {
+//                  //quizBrain.nextQuestion();
+//                  quizBrain.reset();
+//
+//                  Navigator.pop(context);
+//                },
+//              )
+//            ]).show();
+//
+//        //TODO Step 4 Part C - reset the questionNumber,
+//        quizBrain.reset();
+//
+//        //TODO Step 4 Part D - empty out the scoreKeeper.
+//        // scoreKeeper = [];
+//      }
+
+      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
+//      else {
+//        if (userPickedAnswer == quizBrain.getGoldAnswer()) {
+////          myChoice = "gold";
+//          Alert(
+//              context: context,
+//              title: 'Yay!',
+//              desc: 'You\'ve Scored a gold',
+//              content: Column(
+//                children: <Widget>[
+//                  Icon(
+//                    Icons.star,
+//                    color: Colors.yellowAccent,
+//                  )
+//                ],
+//              ),
+//              buttons: [
+////                DialogButton(
+////                  child: Text("Prev"),
+////                  onPressed: () {
+////                    quizBrain.prevQuestion();
+////                    Navigator.pop(context);
+////                  },
+////                ),
+//                DialogButton(
+//                  child: Text("Next"),
+//                  onPressed: () {
+//                    //quizBrain.nextQuestion();
+//                    Navigator.pop(context);
+//                  },
+//                )
+//              ]).show();
+//        } else if (userPickedAnswer == quizBrain.getSilverAnswer()) {
+////          myChoice = "silver";
+//          Alert(
+//              context: context,
+//              title: 'Wow!',
+//              desc: 'You\'ve Scored a Silver',
+//              content: Column(
+//                children: <Widget>[
+//                  Icon(
+//                    Icons.star,
+//                    color: Colors.grey,
+//                  )
+//                ],
+//              ),
+//              buttons: [
+//                DialogButton(
+//                  child: Text("Next"),
+//                  onPressed: () {
+//                    //quizBrain.nextQuestion();
+//                    Navigator.pop(context);
+//                  },
+//                )
+//              ]).show();
+//        } else if (userPickedAnswer == quizBrain.getBronzeAnswer()) {
+////          myChoice = "bronze";
+//          Alert(
+//              context: context,
+//              title: 'Good!',
+//              desc: 'You\'ve Scored a Bronze',
+//              content: Column(
+//                children: <Widget>[
+//                  Icon(
+//                    Icons.star,
+//                    color: Colors.orange,
+//                  )
+//                ],
+//              ),
+//              buttons: [
+//                DialogButton(
+//                  child: Text("Next"),
+//                  onPressed: () {
+//                    //quizBrain.nextQuestion();
+//                    Navigator.pop(context);
+//                  },
+//                )
+//              ]).show();
+//        }
+//        quizBrain.nextQuestion();
+//      }
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: Text('Manners Matters'),
-          backgroundColor: Colors.grey,
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              flex: 5,
-              child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Center(
-                  child: Text(
-                    quizBrain.getQuestionText(),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(15.0),
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.grey)),
-                  textColor: Colors.white,
-                  color: Colors.grey,
-                  child: Text(
-                    quizBrain.getAnswers(0),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                    ),
-                  ),
-                  onPressed: () {
-                    //The user picked true.
-                    checkAnswer(quizBrain.getAnswers(0));
-                    _ratingBar(myChoice);
-                    //quizBrain.nextQuestion();
-                  },
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(15.0),
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.grey)),
-                  color: Colors.grey,
-                  child: Text(
-                    quizBrain.getAnswers(1),
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  onPressed: () {
-                    //The user picked false.
-                    checkAnswer(quizBrain.getAnswers(1));
-                    _ratingBar(myChoice);
-                    //quizBrain.nextQuestion();
-                  },
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(15.0),
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.grey)),
-                  color: Colors.grey,
-                  child: Text(
-                    quizBrain.getAnswers(2),
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  onPressed: () {
-                    //The user picked false.
-                    checkAnswer(quizBrain.getAnswers(2));
-                    _ratingBar(myChoice);
-                    //quizBrain.nextQuestion();
-                  },
-                ),
-              ),
-            ),
-//        Expanded(child: _ratingBar(myChoice))
+  String getQuestionNo() {
+    return 'Question: ' + quizBrain.getTotalQuestionsAsked().toString();
+  }
 
-//        Row(
-//          children: scoreKeeper,
-//        )
-//          Row(
-//            crossAxisAlignment: CrossAxisAlignment.center,
-//            mainAxisAlignment: MainAxisAlignment.center,
-//            children: <Widget>[
-//              SmoothStarRating(
-//                allowHalfRating: true,
-//                starCount: 1,
-//                size: 20,
-//                color: Colors.blue,
-//                borderColor: Colors.white,
-//              )
-//            ],
-//          ),
-          ],
+  FlatButton createChoiceButton(int btnNo) {
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          checkAnswer(quizBrain.getAnswers(btnNo));
+        });
+      },
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+          side: BorderSide(color: Colors.grey)),
+      color: themeColors.burlyWood[900],
+      child: Text(
+        quizBrain.getChoice(btnNo),
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
         ),
       ),
     );
   }
 
-  Widget _ratingBar(String myChoice) {
-    switch (myChoice) {
-      case "gold":
-        return RatingBar(
-            itemCount: 1,
-            itemPadding: EdgeInsets.symmetric(),
-            itemBuilder: (context, index) {
-              return Icon(
-                Icons.star,
-                color: Colors.yellowAccent,
-              );
-            });
-      case "silver":
-        return RatingBar(
-            itemCount: 1,
-            itemPadding: EdgeInsets.symmetric(),
-            itemBuilder: (context, index) {
-              return Icon(
-                Icons.star,
-                color: Colors.grey,
-              );
-            });
-      case "bronze":
-        return RatingBar(
-            itemCount: 1,
-            itemPadding: EdgeInsets.symmetric(),
-            itemBuilder: (context, index) {
-              return Icon(
-                Icons.star,
-                color: Colors.orange,
-              );
-            });
-      default:
-        return Container();
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              getQuestionNo(),
+              style: TextStyle(
+                color: themeColors.vividBurgundy[900],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ), //Score Data here
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: createChoiceButton(0),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          flex: 2,
+          child: createChoiceButton(1),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          flex: 2,
+          child: createChoiceButton(2),
+        ),
+      ],
+    );
   }
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
 }
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,8 +1,10 @@
 import 'question.dart';
+import 'dart:math';
 
 class QuizBrain {
   int _questionNumber = 0;
-
+  int _totalQuestionsAsked = 1;
+  //int qnum = 0;
   List<Question> _questionBank = [
     Question(
         'What do you do with waste paper?',
@@ -23,42 +25,72 @@ class QuizBrain {
         'Ask mommy/daddy for an activity',
         'Eat candies'),
   ];
+  List<int> questionsAsked = [];
 
   void nextQuestion() {
-    if (_questionNumber < _questionBank.length - 1) {
-      print(_questionNumber.toString() + 'in nextQuestion');
-      print(_questionBank.length.toString() + ' _questionBank.length');
-      _questionNumber++;
-      print(_questionNumber.toString() + 'in nextQuestion');
+//    if (_questionNumber < _questionBank.length - 1) {
+//      print(_questionNumber.toString() + 'in nextQuestion');
+//      print(_questionBank.length.toString() + ' _questionBank.length');
+//      _questionNumber++;
+//      print(_questionNumber.toString() + 'in nextQuestion');
+//    }
+    this._questionNumber = Random().nextInt(_questionBank
+        .length); //This whole segment gets a random generated index for the question to be asked
+
+    while (questionsAsked.contains(this._questionNumber)) {
+      this._questionNumber = Random().nextInt(_questionBank.length);
     }
+    questionsAsked.add(
+        this._questionNumber); //Add this question index into the asked list
+    print(_questionNumber.toString() +
+        ' was asked, questions asked so far ' +
+        questionsAsked.length.toString());
   }
 
   void prevQuestion() {
     if (_questionNumber < _questionBank.length - 1) {
-      print(_questionNumber.toString() + 'in prevQuestion');
       _questionNumber--;
-      print(_questionNumber.toString() + 'in prevQuestion');
     }
   }
 
+  int questionBankLength() {
+    return _questionBank.length;
+  }
+
   String getQuestionText() {
-    print(_questionNumber.toString() + 'in getQuestionText');
     return _questionBank[_questionNumber].questionText;
   }
 
   String getQuestionText1(int index) {
-    print(_questionNumber.toString() + 'in getQuestionText1');
     return _questionBank[index].questionText;
   }
 
   String getAnswers(int index) {
-    print(_questionNumber.toString() + 'in getAnswers');
     return _questionBank[_questionNumber].answers[index];
   }
 
 //  bool getCorrectAnswer() {
 //    return _questionBank[_questionNumber].questionAnswer;
 //  }
+
+  String checkAnswer(String selection) {
+    //bool isCorrect = false;
+    String star;
+    if (selection == _questionBank[_questionNumber].goldAnswer) {
+      // _score++;
+      //isCorrect = true;
+      star = "gold";
+      print('picked the gold star ');
+    } else if (selection == _questionBank[_questionNumber].silverAnswer) {
+      star = "silver";
+      print('picked the silver star ');
+    } else if (selection == _questionBank[_questionNumber].bronzeAnswer) {
+      star = "bronze";
+      print('picked the bronze star ');
+    }
+    _totalQuestionsAsked++; //regardless, question has been asked and button has been pressed
+    return star;
+  }
 
   String getGoldAnswer() {
     print(_questionNumber.toString() + 'in getGoldAnswer');
@@ -87,8 +119,30 @@ class QuizBrain {
     }
   }
 
+  String getChoice(int butChoice) {
+    String choiceText;
+    switch (butChoice) {
+      case 0:
+        choiceText = _questionBank[_questionNumber].goldAnswer;
+        break;
+      case 1:
+        choiceText = _questionBank[_questionNumber].silverAnswer;
+        break;
+      case 2:
+        choiceText = _questionBank[_questionNumber].bronzeAnswer;
+        break;
+    }
+    return choiceText;
+  }
+
+  int getTotalQuestionsAsked() {
+    return _totalQuestionsAsked;
+  }
+
   //TODO: Step 4 part B - Create a reset() method here that sets the questionNumber back to index 0.
   void reset() {
     _questionNumber = 0;
+    _totalQuestionsAsked = 1;
+    questionsAsked.clear();
   }
 }
